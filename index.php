5872993c778b99065ae0dbabfdd9e9482ec24f97
24999tnw <153153882+24999tnw@users.noreply.github.com>
2024-01-19 11:21:29 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,30 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>PHP</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+  </head>
+  <body style="background-color: aqua;" >
+    <h1 class="fs-1 text-danger text-center"> 
+      <?php echo "แนะนำตัวเอง"?></h1>
+    <h1 class= "fs-2 text-primary-emphasis text-center">
+      <?php echo "นางสาว ปทิตตา ข่วงทิพย์ ม.5/6 เลขที่20 อายุ17"?> </h1>
+    <h1 class="fs-1 text-light text-center">
+      <?php echo "อาหารที่ชอบ"?></h1>
+    <h1 class= "fs-3 text-primary-emphasis text-center"> 
+      <?php echo "ข้าวผัด"?> </h1>
+    <h1 class="fs-1 text-light text-center"> 
+      <?php echo "กีฬาที่ชอบ"?></h1>
+    <h1 class= "fs-3 text-primary-emphasis text-center">
+      <?php echo "แบดมินตัน"?></h1>
+    <h1 class="fs-1 text-light text-center"> 
+      <?php echo "สีที่ชอบ"?></h1>
+    <h1 class= "fs-3 text-primary-emphasis text-center">
+      <?php echo "สีม่วง"?></h1>
+      <img src="./toothless-dancing-toothless.gif" class="rounded mx-auto d-block" alt="">
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+  </body>
+</html>
